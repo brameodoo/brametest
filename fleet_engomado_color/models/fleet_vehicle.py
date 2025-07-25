9490150a8a98732045fb9d5a060f2548c4daa472
brameodoo <brame.odoo@gmail.com>
2025-07-25 09:23:20 -0600

    Update fleet_vehicle.py

--- fleet_engomado_color/models/fleet_vehicle.py
+++ fleet_engomado_color/models/fleet_vehicle.py
@@ -6,11 +6,11 @@ class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
     engomado_color = fields.Selection([
-        ('amarillo', 'Amarillo (5 o 6)'),
-        ('rosa', 'Rosa (7 o 8)'),
-        ('rojo', 'Rojo (3 o 4)'),
-        ('verde', 'Verde (1 o 2)'),
-        ('azul', 'Azul (9 o 0)'),
+        ('amarillo', 'Amarillo (5 o 6) Lunes'),
+        ('rosa', 'Rosa (7 o 8) Martes'),
+        ('rojo', 'Rojo (3 o 4) Miercoles'),
+        ('verde', 'Verde (1 o 2) Jueves'),
+        ('azul', 'Azul (9 o 0) Viernes'),
     ], string='Color de Engomado',
         help="Color del engomado del vehículo para el programa Hoy No Circula (CDMX).",
         tracking=True)
